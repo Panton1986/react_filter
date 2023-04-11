45de81253886d6dafc18d7dccca904d77814dbaa
Anton Potapov <panton160786@gmail.com>
2023-04-11 21:23:42 +0930

    requests from https

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -12,16 +12,24 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=all&y=2023`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=all&y=2023`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
+            .catch((err) => {
+                console.error(err);
+                this.setState({loading: false})
+            })
     }
 
     searchMovies = (searchStr, type = 'all') => {
         this.setState({loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchStr}${type !== 'all' ? `&type=${type}` : ''}`)
+        fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchStr}${type !== 'all' ? `&type=${type}` : ''}`)
         .then(response => response.json())
         .then(data => this.setState({movies: data.Search, loading: false}))
+        .catch((err) => {
+            console.error(err);
+            this.setState({loading: false})
+        })
     }
 
     render() {
